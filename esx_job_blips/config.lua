b8ed16a2e3dd16ff649e8557aa6c65b4baec4b0f
Lejgi <138068038+Lejgi@users.noreply.github.com>
2025-06-16 19:31:32 +0200

    Add ESX job-based blip script

---
+++ esx_job_blips/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+-- Job required to see the blips
+Config.JobName = 'police'
+
+-- Blip settings
+Config.Sprite = 60
+Config.Color = 3
+
+-- Coordinates for blips
+Config.Locations = {
+    vector3(440.0, -982.0, 30.7),
+    vector3(452.0, -990.0, 30.7),
+    vector3(425.0, -975.0, 30.7)
+}
